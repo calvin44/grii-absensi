48025aa3c86f2e15b6a29ffa97be0d8cb84b8838
calvin44 <calvin.fredicson@gmail.com>
2024-01-04 01:29:28 +0800

    refactor: TextInput custom component

---
+++ src/components/textInput.tsx
@@ -0,0 +1,30 @@
+import { FormData } from '@/pages/addJemaatBaru'
+import { RegisterOptions, UseFormReturn, ValidationRule, useForm } from 'react-hook-form'
+
+interface TextInputProps {
+  name: keyof FormData
+  labelText: string
+  methods: UseFormReturn<FormData>,
+  validationRule?: RegisterOptions<FormData, keyof FormData>
+  pattern?: ValidationRule<RegExp> | undefined
+}
+
+const TextInput: React.FC<TextInputProps> = ({ methods, labelText, name, validationRule, pattern }) => {
+  return (
+    <div className="mb-4">
+      <label className="block text-sm font-medium text-gray-700 mb-3">
+        {labelText}
+      </label>
+      <input
+        type="text"
+        className="mt-1 p-3 w-full rounded-md border focus:outline-none focus:border-teal-300"
+        placeholder={labelText}
+        {...methods.register(name, validationRule)}
+        onBlur={() => methods.trigger(name)}
+      />
+      {methods.formState.errors?.[name] && <p className="text-red-500 text-sm mt-1">{methods.formState.errors?.[name]?.message}</p>}
+    </div>
+  )
+}
+
+export default TextInput
--- src/pages/addJemaatBaru.tsx
+++ src/pages/addJemaatBaru.tsx
@@ -3,7 +3,7 @@ import PageContainer from "@/components/pageContainer"
 import DatePicker from "@/components/dateInput"
 import TextInput from '@/components/textInput'
 
-interface FormData {
+export interface FormData {
   tanggalDatang: string
   namaLengkap: string
   noHP: string
@@ -25,38 +25,29 @@ const AddJemaatBaru: React.FC = () => {
           <h2 className="text-4xl font-extrabold text-gray-800 mb-8 text-center">Add Jemaat Baru</h2>
           <form className="flex flex-col gap-6" onSubmit={handleSubmit(onSubmit)}>
             <DatePicker name="tanggalDatang" title="Tanggal datang gereja" placeholderText="Pilih tanggal datang gereja" />
-            <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700 mb-3">
-                Nama Lengkap
-              </label>
-              <input
-                type="text"
-                className="mt-1 p-3 w-full rounded-md border focus:outline-none focus:border-teal-300"
-                placeholder="Nama Lengkap"
-                {...methods.register("namaLengkap", { required: "Nama Lengkap is required" })}
-                onBlur={() => methods.trigger("namaLengkap")}
-              />
-              {methods.formState.errors?.namaLengkap && <p className="text-red-500 text-sm mt-1">{methods.formState.errors?.namaLengkap?.message}</p>}
-            </div>
-            <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700 mb-3">
-                No HP
-              </label>
-              <input
-                type="text"
-                className="mt-1 p-3 w-full rounded-md border focus:outline-none focus:border-teal-300"
-                placeholder="No HP"
-                {...methods.register("noHP", {
-                  required: "No HP is required",
-                  pattern: {
-                    value: /^\d{10}$/,
-                    message: "Please enter a valid 10-digit phone number.",
-                  }
-                })}
-                onBlur={() => methods.trigger("noHP")}
-              />
-              {methods.formState.errors?.noHP && <p className="text-red-500 text-sm mt-1">{methods.formState.errors?.noHP?.message}</p>}
-            </div>
+            <TextInput
+              name="namaLengkap"
+              labelText="Nama Lengkap"
+              methods={methods}
+              validationRule={{ required: "Nama lengkap is required" }}
+            />
+            <TextInput
+              name="noHP"
+              labelText="Nomor HP"
+              methods={methods}
+              validationRule={{
+                pattern: {
+                  value: /^\d{10}$/,
+                  message: "Please enter a valid 10-digit phone number.",
+                }
+              }}
+            />
+            <TextInput
+              name="lineID"
+              labelText="Line ID"
+              methods={methods}
+              validationRule={{ required: "Line ID is required" }}
+            />
             <button
               type="submit"
               className="w-full bg-teal-500 text-white p-3 rounded-md hover:bg-teal-600 focus:outline-none focus:ring focus:border-teal-300"
